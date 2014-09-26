809fb7cf99fa39aaf5dd743b490319394008c4a5
Stephen <stephenp182@gmail.com>
2014-09-26 15:45:43 +0100

    edit

--- commandscript.sh
+++ commandscript.sh
@@ -1,1 +1,13 @@
-grep -o "processor" /proc/cpuinfo | wc -w
+#!/bin/bash
+
+
+# Title : Sally Simon Runs On How Many Processors?
+# Date : 26/09/14
+# Author : Stephen Plummer
+# Version : 0.1.1
+# Description : CPU count script
+# Options : 
+
+echo "Sally Simmon Runs On How Many Processors?"
+
+grep -o "processor" /proc/cpuinfo | wc -l
